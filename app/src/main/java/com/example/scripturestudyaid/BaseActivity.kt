ef22382a657f2946ae6740417b7bf8b856f05c06
Bryson Cotton <85445857+brysoncotton@users.noreply.github.com>
2026-02-10 21:35:17 -0800

    potential fix for samsung screen layout

--- app/src/main/java/com/example/scripturestudyaid/BaseActivity.kt
+++ app/src/main/java/com/example/scripturestudyaid/BaseActivity.kt
@@ -14,6 +14,30 @@ open class BaseActivity : AppCompatActivity() {
         applyTheme()
     }
 
+    override fun setContentView(layoutResID: Int) {
+        super.setContentView(layoutResID)
+        applyWindowInsets()
+    }
+
+    override fun setContentView(view: android.view.View?) {
+        super.setContentView(view)
+        applyWindowInsets()
+    }
+
+    override fun setContentView(view: android.view.View?, params: ViewGroup.LayoutParams?) {
+        super.setContentView(view, params)
+        applyWindowInsets()
+    }
+
+    private fun applyWindowInsets() {
+        val rootView = findViewById<android.view.View>(android.R.id.content)
+        androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
+            val systemBars = insets.getInsets(androidx.core.view.WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
     protected fun applyTheme() {
         val prefs: SharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE)
         val bgColor = prefs.getString("bgColor", "System")
@@ -27,10 +51,7 @@ open class BaseActivity : AppCompatActivity() {
             "Black" -> rootView.setBackgroundColor(Color.BLACK)
             "Dark Blue" -> rootView.setBackgroundColor(Color.parseColor("#1a237e")) // Dark blue
             else -> {
-                // System default - usually determined by theme, might need resetting if changing back
-                // For now, we won't force reset to system drawable to avoid complexity, 
-                // but setting background to null or a default color logic could go here.
-                // If we assume light theme is default:
+                // System default
                  rootView.setBackgroundColor(Color.WHITE) 
             }
         }
@@ -45,13 +66,6 @@ open class BaseActivity : AppCompatActivity() {
                 "Black" -> view.setTextColor(Color.BLACK)
                 "White" -> view.setTextColor(Color.WHITE)
                 "Gold" -> view.setTextColor(Color.parseColor("#b5884f"))
-                else -> {
-                     // Keep default or system text color
-                     // To strictly enforce system default we'd need to invalidate or re-inflate, 
-                     // but for this simple implementation we might just leave it 
-                     // OR force black/white based on presumed background.
-                     // For now, do nothing if "System" to let layout XML decide.
-                }
             }
         }
 
